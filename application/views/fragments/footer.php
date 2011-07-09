bcf3d64d3ce0faacfdac40b53b2a0550fc6eb65c
Sammy Hubner <sammy@subrise.com>
2011-07-09 13:38:53 +0200

    Updated to a newer jquery version

--- application/views/fragments/footer.php
+++ application/views/fragments/footer.php
@@ -1,9 +1,7 @@
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
-	<script>window.jQuery || document.write("<script src='/assets/js/libs/jquery-1.5.1.min.js'>\x3C/script>")</script>
-
-
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write("<script src='/assets/js/libs/jquery-1.6.2.min.js'>\x3C/script>")</script>
+	
 	<!-- scripts concatenated and minified via ant build script-->
-	<script src="/assets/js/plugins.js"></script>
 	<script src="/assets/js/script.js"></script>
 	<!-- end scripts-->
 
